3f36f52f73f9a5c5cc97caad9aff2c1d20834c2d
Pranav Gaikwad <pranavjigaikwad@gmail.com>
2021-06-17 21:17:28 +0530

    more than half completed login and sign up done profile now fetch data after login/sign up app now requires internet connection to run .changes done in manifest
    added :
    user info page
    user features page
    will all backend
    
    advancement made
    started to create new app,
    features are now uploded to db,
    image select feature added
    
    my app section created
    image can now be uploaded
    
    all set

--- app/src/main/java/com/example/androidautocoder/usersAppDownloadPage.java
+++ app/src/main/java/com/example/androidautocoder/usersAppDownloadPage.java
@@ -1,14 +1,51 @@
 package com.example.androidautocoder;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class usersAppDownloadPage extends AppCompatActivity {
 
+    private View decorView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_app_download_page);
+
+
+
+        //        hide bars
+        decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if (visibility == 0) {
+                    decorView.setSystemUiVisibility(hideSystemBars());
+                }
+            }
+        });
     }
+
+
+    //        hide bars
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(hideSystemBars());
+        }
+    }
+
+    private int hideSystemBars() {
+        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+    }
+    //    end hide bar
+
 }
