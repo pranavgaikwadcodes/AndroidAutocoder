90ff03b4b8abf09873f27c93bea6fdb890e189a5
Pranav Gaikwad <pranavjigaikwad@gmail.com>
2021-06-17 15:11:12 +0530

    more than half completed login and sign up done profile now fetch data after login/sign up app now requires internet connection to run .changes done in manifest
    added :
    user info page
    user features page
    will all backend
    
    advancement made
    started to create new app,
    features are now uploded to db,
    my app section created

--- app/src/main/java/com/example/androidautocoder/AppEditPage.java
+++ app/src/main/java/com/example/androidautocoder/AppEditPage.java
@@ -24,9 +24,9 @@ import java.util.HashMap;
 public class AppEditPage extends AppCompatActivity {
     private View decorView;
 
-    Button addBlog2, save;
-    TextInputLayout blog2, CompanyName, AppDesc, CompanyDesc, Website, AboutUs, Fb, LinkedIn, Twitter, Email, BlogURL, adminUser, adminPass;
-    TextView appNameHere;
+    Button addBlog2, save ,uploadImageBtn;
+    TextInputLayout blog2, CompanyName, AppDesc, CompanyDesc, Website, AboutUs, Fb, LinkedIn, Twitter, Email, BlogURL, adminUser, adminPass ,userAddress, userVideo,userDonateLink;
+    TextView appNameHere,adminpanelsettingstxt;
 
     //    database
     FirebaseDatabase rootNode;
@@ -37,6 +37,8 @@ public class AppEditPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_edit_page);
 
+        addBlog2 = (Button) findViewById(R.id.blogAddBtn);
+        adminpanelsettingstxt = (TextView)findViewById(R.id.aps);
 
         retriveEverythingFromDB();
 
@@ -52,16 +54,38 @@ public class AppEditPage extends AppCompatActivity {
         BlogURL = findViewById(R.id.blogURL1);
         adminUser = findViewById(R.id.setUsername);
         adminPass = findViewById(R.id.setPassword);
-        Twitter = findViewById(R.id.twitter);
+        Twitter = findViewById(R.id.twitterURL);
         blog2 = findViewById(R.id.blogURL2);
+        userAddress = findViewById(R.id.userAddress);
+        userVideo = findViewById(R.id.videoFromUser);
+        uploadImageBtn = findViewById(R.id.uploadImg);
+        userDonateLink = findViewById(R.id.userDonateLink);
 
+//        hide all edittext
+        Website.setVisibility(View.GONE);
+        AboutUs.setVisibility(View.GONE);
+        Fb.setVisibility(View.GONE);
+        LinkedIn.setVisibility(View.GONE);
+        Twitter.setVisibility(View.GONE);
+        Email.setVisibility(View.GONE);
+        BlogURL.setVisibility(View.GONE);
+        adminUser.setVisibility(View.GONE);
+        adminPass.setVisibility(View.GONE);
+        adminpanelsettingstxt.setVisibility(View.GONE);
+        userAddress.setVisibility(View.GONE);
+        userVideo.setVisibility(View.GONE);
+        uploadImageBtn.setVisibility(View.GONE);
+        userDonateLink.setVisibility(View.GONE);
+
+        blog2.setVisibility(View.GONE);
+        addBlog2.setVisibility(View.GONE);
 
         save = (Button) findViewById(R.id.save);
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent next = new Intent(getApplicationContext(),AppEditPage2.class);
+                Intent next = new Intent(getApplicationContext(), AppEditPage2.class);
                 next.putExtra("app_NamE", appNameHere.getText().toString());
                 next.putExtra("app_Desc", AppDesc.getEditText().getText().toString());
                 next.putExtra("Company_Name", CompanyName.getEditText().getText().toString());
@@ -81,7 +105,7 @@ public class AppEditPage extends AppCompatActivity {
         });
 
 
-        addBlog2 = (Button) findViewById(R.id.blogAddBtn);
+
         addBlog2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -131,7 +155,7 @@ public class AppEditPage extends AppCompatActivity {
                 String appDesc = snapshot.child("appDesc").getValue().toString();
                 String companyDesc = snapshot.child("appName").getValue().toString();
                 String adminPanel = snapshot.child("adminPanel").getValue().toString();
-                String aboutUs = snapshot.child("appName").getValue().toString();
+                String aboutUs = snapshot.child("aboutUs").getValue().toString();
                 String notification = snapshot.child("notification").getValue().toString();
                 String share = snapshot.child("share").getValue().toString();
                 String review = snapshot.child("review").getValue().toString();
@@ -154,6 +178,47 @@ public class AppEditPage extends AppCompatActivity {
                 AppDesc.getEditText().setText(appDesc);
                 CompanyName.getEditText().setText(companyName);
                 CompanyDesc.getEditText().setText(companyDesc);
+
+                if (website.equals("yes")) {
+                    Website.setVisibility(View.VISIBLE);
+                }
+                if (aboutUs.equals("yes")) {
+                    AboutUs.setVisibility(View.VISIBLE);
+                }
+                if (adminPanel.equals("yes")) {
+                    adminpanelsettingstxt.setVisibility(View.VISIBLE);
+                    adminUser.setVisibility(View.VISIBLE);
+                    adminPass.setVisibility(View.VISIBLE);
+                }
+                if (blog.equals("yes")) {
+                    BlogURL.setVisibility(View.VISIBLE);
+                    addBlog2.setVisibility(View.VISIBLE);
+
+                }
+                if (fb.equals("yes")) {
+                    Fb.setVisibility(View.VISIBLE);
+                }
+                if (linkedin.equals("yes")) {
+                    LinkedIn.setVisibility(View.VISIBLE);
+                }
+                if (email.equals("yes")) {
+                    Email.setVisibility(View.VISIBLE);
+                }
+                if (twitter.equals("yes")) {
+                    Twitter.setVisibility(View.VISIBLE);
+                }
+                if (map.equals("yes")) {
+                    userAddress.setVisibility(View.VISIBLE);
+                }
+                if (uploadVideo.equals("yes")) {
+                    userVideo.setVisibility(View.VISIBLE);
+                }
+                if (uploadPhoto.equals("yes")) {
+                    uploadImageBtn.setVisibility(View.VISIBLE);
+                }
+                if (donate.equals("yes")) {
+                    userDonateLink.setVisibility(View.VISIBLE);
+                }
 
             }
 
